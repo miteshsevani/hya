4b1423a780a9cebee43498c97d47d744d6ab5d7a
Mitesh Sevani <msevani@gmail.com>
2020-08-03 14:29:19 +0100

    Updated nav link

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,6 @@ import { HashRouter, NavLink } from "react-router-dom";
 import '../stylesheets/header.css'
 import '../stylesheets/nav.css'
 
-import Nav from './Nav'
-
 export default class Header extends React.Component {
   state = {
     toggle: false,
@@ -40,7 +38,7 @@ export default class Header extends React.Component {
                 <li><NavLink onClick={this.handleClick} to="/keydates">Key Dates</NavLink></li>
                 <li><NavLink onClick={this.handleClick} to="/history">History</NavLink></li>
                 <li><a onClick={this.handleClick} href="http://hyauk.org/moodle" target="_blank">Moodle</a></li>
-                <li><NavLink onClick={this.handleClick} to="/register">Register</NavLink></li>
+                <li><NavLink onClick={this.handleClick} to="/register">Registration</NavLink></li>
               </ul>
               <div className="social">
                 <a href="https://www.facebook.com/hyauk1" target="_blank" rel="noopener noreferrer" className="facebook"></a>
--- src/components/Register.js
+++ src/components/Register.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Register = () => {
   return(
     <div className="content">
-      <h1 className="title">Register</h1>
+      <h1 className="title">Registration</h1>
       <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfSxxIMZF4aJEYkKCogqf16WlsyLcieKdBc6ylqM3aFKMmd7A/viewform?embedded=true" width="100%" height="520" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
     </div>
   )
