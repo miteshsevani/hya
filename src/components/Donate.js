1cabdd4ba90bba06ff7124ab0c3b8e5b42178667
Mitesh Sevani <msevani@gmail.com>
2021-10-02 12:49:01 +0100

    Content updates

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -4,9 +4,25 @@ const Donate = () => {
   return(
     <div className="content">
       <h1 className="title">Donate</h1>
-      <h3>Hindu Youth Association</h3>
-      <h3>A/C no: 64283224</h3>
-      <h3>Sort code: 60-24-26</h3>
+
+      <h3>Donate via online banking</h3><br/>
+
+      <table>
+        <tr>
+          <td>Account</td>
+          <td>Hindu Youth Association</td>
+        </tr>
+        <tr>
+          <td>A/C no</td>
+          <td>64283224</td>
+        </tr>
+        <tr>
+          <td>Sort code</td>
+          <td>60-24-26</td>
+        </tr>
+      </table>
+
+      <p>For the payment reference, please use your <b>FIRSTNAME_SURNAME_POSTCODE</b></p>
     </div>
   )
 }
--- src/components/Register.js
+++ src/components/Register.js
@@ -4,7 +4,9 @@ const Register = () => {
   return(
     <div className="content">
       <h1 className="title">Registration</h1>
-      <iframe
+
+      <p>If you are interested in attending any of the classes please email <a href='mailto:info@hyauk.org' target='_blank'>info@hyauk.org</a></p>
+      {/*<iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSfSxxIMZF4aJEYkKCogqf16WlsyLcieKdBc6ylqM3aFKMmd7A/viewform?embedded=true"
         width="100%"
         height="520"
@@ -13,7 +15,7 @@ const Register = () => {
         marginwidth="0"
         title="Registration Form">
           Loading…
-      </iframe>
+      </iframe>*/}
     </div>
   )
 }
